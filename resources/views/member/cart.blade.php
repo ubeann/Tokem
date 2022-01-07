075bf452e326dcbff3484f7359116dc781d97b66
adiharka <andimahardika73@gmail.com>
2022-01-07 09:58:58 +0700

    Add and improve some pages

--- resources/views/member/cart.blade.php
+++ resources/views/member/cart.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <form class="container bg-trasparent my-4 p-3" style="position: relative;">
-    <h2>Your Cart</h2>
+    <h2>Please confirm your order</h2>
     <table class="table table-striped table-hover">
         <thead>
         <tr>
@@ -18,28 +18,35 @@
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Rp 12.000,00</td>
-            <td>5</td>
+            <td><input name="phone" type="number" value="" class="form-control form-control-sm""></td>
             <td class="text-end">Rp 60.000,00</td>
         </tr>
         <tr>
             <th scope="row">2</th>
             <td>Jacob</td>
             <td>Rp 12.000,00</td>
-            <td>5</td>
+            <td><input name="phone" type="number" value="" class="form-control form-control-sm""></td>
             <td class="text-end">Rp 60.000,00</td>
         </tr>
         <tr>
             <th scope="row">3</th>
             <td>Jacob</td>
             <td>Rp 12.000,00</td>
-            <td>5</td>
+            <td><input name="phone" type="number" value="" class="form-control form-control-sm""></td>
             <td class="text-end">Rp 60.000,00</td>
         </tr>
+        <td><input name="phone" type="number" value="" class="form-control form-control-sm""></td>
         <tr>
             <td colspan="5" class="text-end"><h5>Rp 350.000,00</h5></td>
         </tr>
         </tbody>
     </table>
-    <button type="submit" class="btn btn-primary">Checkout</button>
+    <a href="{{ route('member.checkout') }}" type="submit" class="btn btn-primary">Checkout</a>
 </form>
+
+<style>
+    .form-control {
+        max-width: 55px;
+    }
+</style>
 @endsection
